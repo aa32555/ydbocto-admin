e852fbbfb4528cb3f1806189cfb52eddbe99fbb3
Jon Badiali <jon@yottadb.com>
2019-06-21 17:11:24 -0400

    Added copyright information

--- cmd/ydbocto-admin/main.go
+++ cmd/ydbocto-admin/main.go
@@ -1,17 +1,24 @@
+/****************************************************************
+ *								                                *
+ * Copyright (c) 2019 YottaDB LLC and/or its subsidiaries.	    *
+ * All rights reserved.						                    *
+ *								                                *
+ *	This source code contains the intellectual property	        *
+ *	of its copyright holder(s), and is made available	        *
+ *	under a license.  If you do not know the terms of	        *
+ *	the license, please stop and do not read further.	        *
+ *								                                *
+ ****************************************************************/
+
 package main
 
 import (
 	"fmt"
-	// "strings"
-	// "syscall"
-	// "crypto/md5"
-	// "encoding/hex"
 	"readpassword"
 	"gitlab.com/euterpe/ydbocto-admin/pkg/adduser"
 	"gitlab.com/euterpe/ydbocto-admin/pkg/deleteuser"
 	"gitlab.com/euterpe/ydbocto-admin/pkg/showusers"
 	"github.com/docopt/docopt-go"
-	// "golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
